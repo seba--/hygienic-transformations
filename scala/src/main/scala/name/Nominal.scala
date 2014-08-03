beebf57018713302c08b08f921c1be4c2fc8c22e
Sebastian Erdweg <seba.github@gmail.com>
2014-08-03 03:02:54 +0200

    cleanup

--- scala/src/main/scala/name/Nominal.scala
+++ scala/src/main/scala/name/Nominal.scala
@@ -9,11 +9,9 @@ trait Nominal {
   type Renaming = Name => Name
 
   def allNames: Set[Name]
-  def rename(renaming: Map[Name.ID,Name]): Nominal = rename(name => renaming.getOrElse(name.id,name))
   def rename(renaming: Renaming): Nominal
   def resolveNames: NameGraph
-}
-object Nominal {
-  def isName(req: Name, was: Name): Option[String] =
-    if (req == was) Some(was.name) else None
+
+  def rename(renaming: Map[Name.ID,Name]): Nominal =
+    rename(name => renaming.getOrElse(name.id,name))
 }
